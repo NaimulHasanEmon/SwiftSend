1f8d44a06aa543a407fd163d08cb5b0342bd7337
emon12700 <hasan15-5601@diu.edu.bd>
2024-03-07 21:16:22 +0600

    starting from new

---
+++ Js/script.js
@@ -0,0 +1,9 @@
+function handleConnectNow() {
+    document.getElementById("btn-next");
+    window.location.href = "connectNow.html";
+}
+
+function handleHomePage() {
+    document.getElementById("btn-next");
+    window.location.href = "homePage.html";
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Website Link:"https://naimulhasanemon.github.io/Software-Project-3/
